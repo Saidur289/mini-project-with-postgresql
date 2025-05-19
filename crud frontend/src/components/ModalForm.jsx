95a46da9faf8078dfc989bc7f72dfac52cf2c394
Saidur289 <saidur.riaz1@gmailcomm>
2025-05-19 17:17:44 +0600

    polish my ui and declare state manage state

--- crud frontend/src/components/ModalForm.jsx
+++ crud frontend/src/components/ModalForm.jsx
@@ -1,4 +1,19 @@
-const ModalForm = ({ isOpen, mode, onClose, onSubmit }) => {
+import { useState } from "react";
+
+const ModalForm = ({ isOpen, mode, onClose, onSubmits  }) => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [rate, setRate] = useState('')
+  const [status, setStatus] = useState(false)
+  const [job, setJob] = useState('')
+  const handleStatus = (e) => {
+    if(status === 'active'){
+      setStatus(e.target.value)
+    }
+  }
+  const handleSubmit = e => {
+    e.preventDefault()
+  }
   return (
     <>
       <dialog id="my_modal_3" className="modal" open={isOpen}>
@@ -6,23 +21,23 @@ const ModalForm = ({ isOpen, mode, onClose, onSubmit }) => {
           <h1 className="font-bold text-lg py-4">
             {mode === "edit" ? "Edit Client" : "Client Details"}
           </h1>
-          <form method="dialog">
+          <form  onSubmit={handleSubmit} method="dialog">
             {/* if there is a button in form, it will close the modal */}
-            <label className="input input-bordered flex my-4  items-center gap-2">
+            <label className="input input-bordered flex my-4  items-center gap-2" >
               Name
-              <input type="text" className="grow" placeholder="Daisy" />
+              <input value={name} type="text" className="grow" placeholder="Name"  onChange={(e)=> setName(e.target.value)} />
             </label>
             <label className="input input-bordered my-4 flex items-center gap-2">
               Email
-              <input
+              <input value={email}  onChange={(e)=> setEmail(e.target.value)}
                 type="text"
                 className="grow"
-                placeholder="daisy@site.com"
+                placeholder="Your Email"
               />
             </label>
             <label className="input input-bordered flex items-center gap-2 my-4">
               Job
-              <input
+              <input value={job} onChange={(e)=> setJob(e.target.value)}
                 type="text"
                 className="grow"
                 placeholder="daisy@site.com"
@@ -31,9 +46,9 @@ const ModalForm = ({ isOpen, mode, onClose, onSubmit }) => {
             <div className="flex justify-between my-4 items-center gap-2">
               <label className="input input-bordered flex items-center gap-2">
                 Rate
-                <input type="text" className="grow" placeholder="Rate" />
+                <input value={rate} type="text" className="grow" placeholder="Rate" onChange={(e)=> setRate(e.target.value)}/>
               </label>
-              <select className="select select-bordered w-full max-w-xs">
+              <select value={status? 'Active': 'Inactive'} className="select select-bordered w-full max-w-xs" onChange={handleStatus}>
                 <option disabled selected>
                   Select Status
                 </option>
@@ -47,7 +62,7 @@ const ModalForm = ({ isOpen, mode, onClose, onSubmit }) => {
             >
               ✕
             </button>
-            <button className="btn btn-success">
+            <button onClick={onClose} className="btn btn-success">
               {mode === "edit" ? "Save Changes" : "Add Client"}
             </button>
           </form>
