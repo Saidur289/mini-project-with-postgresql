7c284b4cce9acf32b5bdd02ccb8d341165207445
Saidur289 <saidur.riaz1@gmailcomm>
2025-05-21 00:01:49 +0600

    show data tablelist by calling api

--- crud frontend/src/components/TableList.jsx
+++ crud frontend/src/components/TableList.jsx
@@ -1,35 +1,51 @@
+import { useEffect } from "react";
+import { useState } from "react"
+import axios from 'axios'
 
 
 const TableList = ({handleOpen}) => {
-   const clients = [
-  {
-    name: "Joe",
-    work: "freelance-developer",
-    blogs: "54",
-    websites: "32",
-    hackathons: "6",
-    location: "morocco",
-    id: "0",
-    email: "joe@example.com",
-    isActive: true,
-    rate: 101
-  },
-  {
-    name: "janet",
-    work: "fullstack-developer",
-    blogs: "34",
-    websites: "12",
-    hackathons: "8",
-    location: "Mozambique",
-    id: "1",
-    email: "janet@example.com",
-    isActive: false,
-    rate: 103
-  }
-];
+  const [tableData, setTableData] = useState([])
+  const [error, setError] = useState('')
+  useEffect(() => {
+    const fetchData = async() => {
+      try {
+        const response = await axios.get('http://localhost:3000/api/clients')
+        setTableData(response.data)
+      } catch (error) {
+        setError(error.message)
+      }
+    };
+    fetchData()
+  }, [])
+//   {
+//     name: "Joe",
+//     work: "freelance-developer",
+//     blogs: "54",
+//     websites: "32",
+//     hackathons: "6",
+//     location: "morocco",
+//     id: "0",
+//     email: "joe@example.com",
+//     isActive: true,
+//     rate: 101
+//   },
+//   {
+//     name: "janet",
+//     work: "fullstack-developer",
+//     blogs: "34",
+//     websites: "12",
+//     hackathons: "8",
+//     location: "Mozambique",
+//     id: "1",
+//     email: "janet@example.com",
+//     isActive: false,
+//     rate: 103
+//   }
+// ];
 
     return (
        <>
+       {error && <div className="alert alert-error">{error}</div>}
        <div className="overflow-x-auto">
   <table className="table">
     {/* head */}
@@ -45,7 +61,7 @@ const TableList = ({handleOpen}) => {
     <tbody  className="hover">
       {/* row 1 */}
      {
-        clients.map((client, index) => 
+        tableData.map((client, index) => 
                <tr key={client.id}>
         <th>{index + 1}</th>
         <td>{client.name}</td>
